b9641bde6f028e35439d23cff439ce264c035a23
Atul <atulmalviyav2@gmail.com>
2024-09-11 02:40:16 +0530

    clerk implementation

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,19 @@
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./_components/Navbar";
 import { Providers } from "./providers";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +22,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider> 
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className='antialiased'
       >
         <Providers>
           <Navbar />
@@ -31,5 +33,6 @@ export default function RootLayout({ children }) {
         </Providers>
       </body>
     </html>
+    </ClerkProvider> 
   );
 }
